5cf483c42d4c90a96d07982141021cd76e9d8a69
Emi-legui <emilegui76@gmail.com>
2025-09-08 19:08:44 -0300

    Insertando JMenu
    Interfaz Grafica

--- src/newpackage/Menu.java
+++ src/newpackage/Menu.java
@@ -44,10 +44,10 @@ public class Menu extends javax.swing.JFrame {
             .addGap(0, 326, Short.MAX_VALUE)
         );
 
-        jMenu1.setText("File");
+        jMenu1.setText("Alumno");
         jMenuBar1.add(jMenu1);
 
-        jMenu2.setText("Edit");
+        jMenu2.setText("Materia");
         jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
