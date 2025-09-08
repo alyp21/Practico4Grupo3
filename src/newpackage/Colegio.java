6827d3a7ba6b2bbf63d69d775069a087eead7342
alyy <alybelp7@gmail.com>
2025-09-08 16:00:38 -0300

    tengo sueño

---
+++ src/newpackage/Colegio.java
@@ -0,0 +1,16 @@
+
+package newpackage;
+
+public class Colegio {
+
+    public static void main(String[] args) {
+       
+        Materia materia1 = new Materia(123, "Web2",2);
+        Materia materia2 = new Materia(456,"Matematicas",1);
+        Materia materia3 = new Materia(789, "Laboratorio1",1);
+        
+        Alumno alumno1 = new Alumno(1001,"Martin","Lopez");
+        Alumno alumno2 = new Alumno(1002,"Brenda","Martinez");
+    }
+    
+}
